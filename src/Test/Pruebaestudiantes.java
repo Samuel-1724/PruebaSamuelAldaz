19688067dc9b979a3d451fe6ff2c5d1269ddc839
Samuel Aldaz <SAmd2nte@hotmail.com>
2021-06-18 09:53:25 -0500

    v1 creación de prueba

---
+++ src/Test/Pruebaestudiantes.java
@@ -0,0 +1,11 @@
+package Test;
+
+import comandos.Estudiantes;
+
+public class Pruebaestudiantes {
+
+    public static void main(String[] args) {
+    Estudiantes c1 = new Estudiantes();
+    c1.Notas_Alumnos();
+    }
+}
